4bc3c21e64fe2e6aa5b3a914b3875257ec3f0327
Benoit de Bernardy <benoit.debernardy@busbud.com>
2022-03-26 21:15:23 +0100

    parallelism test

---
+++ parallelism_flow.py
@@ -0,0 +1,58 @@
+from asyncio import wait_for
+import os
+import time
+
+import prefect
+from prefect import task, Flow, Parameter
+from prefect.run_configs import UniversalRun
+from prefect.storage import GitHub
+
+from prefect.tasks.kubernetes import CreateNamespacedJob
+
+
+def create_template(name: str):
+    return {
+        "apiVersion": "batch/v1",
+        "kind": "Job",
+        "metadata": {
+            "name": "pi"
+        },
+        "spec": {
+            "template": {
+                "spec": {
+                    "containers": [{
+                        "name": name,
+                        "image": "eu.gcr.io/busbud-integrations/k8-tests:latest"
+                        # "command": ["perl",  "-Mbignum=bpi", "-wle", "print bpi(2000)"]
+                    }],
+                    "restartPolicy": "Never"
+                },
+            },
+            "backoffLimit": 4
+        }
+    }
+
+
+job1 = CreateNamespacedJob(
+    body=create_template("job1"), namespace="ua-prefect", kubernetes_api_key_secret=None)  # type: ignore
+
+job2 = CreateNamespacedJob(
+    body=create_template("job2"), namespace="ua-prefect", kubernetes_api_key_secret=None)  # type: ignore
+
+job3 = CreateNamespacedJob(
+    body=create_template("job2"), namespace="ua-prefect", kubernetes_api_key_secret=None)  # type: ignore
+
+with Flow("parallel-flow") as flow:
+    job1()
+    job2()
+    job3(upstream_tasks=[job1, job2])
+
+# Storing flow in github
+flow.storage = GitHub(
+    repo="bdbernardy/prefect-tutorial",                           # name of repo
+    path="kubernetes_flow.py"                   # location of flow file in repo
+    # access_token_secret="GITHUB_ACCESS_TOKEN"  # name of personal access token secret
+)
+
+# Register the flow under the "tutorial" project
+flow.register(project_name="tutorial")
